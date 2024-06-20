7c088e8d83a06790cb868c9d87ec220b40bfc9d6
dpeng28 <dpeng5@jh.edu>
2024-06-20 12:52:12 -0400

    added analysis

--- CrebA_ChIP_Analysis/workflow/analyze_FIMO_SPCGs.py
+++ CrebA_ChIP_Analysis/workflow/analyze_FIMO_SPCGs.py
@@ -2,32 +2,13 @@ import pandas as pd
 import numpy as np 
 import os
 
-spcg_tab = pd.read_excel("../../SPCG_files/SPCG List.xlsx")
+spcg_tab = pd.read_excel("../input/SPCG_files/SPCG List.xlsx")
 spcg_list = np.unique(spcg_tab['Drosophila FBgn'])
 
-out_path = '../output/SPCG_promoters'
-
-##### look at the CrebA motifs #####
-fimo_directory = "../output/SPCG_promoters/CrebA_motifs"
-folder_list = os.listdir(fimo_directory)
-folder_list = [x for x in folder_list if x != '.DS_Store']
-folder_list = [x for x in folder_list if x != 'motif_database.txt']
-folder_list = [x for x in folder_list if 'untrimmed' not in x]
-
-crebA_df = pd.DataFrame(index = folder_list, columns = spcg_list, data = 0)
-
-for tmp_folder in folder_list: 
-    print(tmp_folder)
-    fimo_results = pd.read_csv(os.path.join(fimo_directory, tmp_folder, 'best_site.narrowPeak'), sep = '\t', header = None)
-    fimo_results = fimo_results.loc[fimo_results[7] < 0.0001, :]
-    fimo_spcgs = fimo_results[0]
-    fimo_spcgs = [x.split("_")[0] for x in fimo_spcgs]
-    crebA_df.loc[tmp_folder, fimo_spcgs] = crebA_df.loc[tmp_folder, fimo_spcgs] + 1
-
-crebA_df.to_csv(os.path.join(out_path, 'crebA_hits_0.0001.csv'))
+out_path = '../output/SPCG_regulatory_regions'
 
 ##### look at all the other TF motifs ######
-fimo_directory = "../output/SPCG_promoters/fly_factor_survey"
+fimo_directory = "../output/SPCG_regulatory_regions/fly_factor_survey"
 folder_list = os.listdir(fimo_directory)
 folder_list = [x for x in folder_list if x != '.DS_Store']
 folder_list = [x for x in folder_list if x != 'motif_database.txt']
@@ -57,16 +38,16 @@ conversion_df = pd.DataFrame(np.array([fly_id_list, gene_id_list]).T, index = fl
 
 TF_df.index = conversion_df.loc[TF_df.index, 0] + " (" + conversion_df.loc[TF_df.index, 1] + ")"
 TF_df[TF_df > 1] = 1
-TF_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_hits_0.0001.csv'))
+TF_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_hits.csv'))
 
 # summarize the results 
 TF_df.sum(axis = 1).sort_values()
 prop_df = pd.DataFrame(data = np.array([TF_df.index, TF_df.sum(axis = 1)]).T, columns = ['TF_motifs', 'num_SPCGs'])
 prop_df = prop_df.sort_values('num_SPCGs')
-prop_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_hits_0.0001_prop.csv'))
+prop_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_hits_prop.csv'))
 
 ##### look at all the TF motifs combined #####
-fimo_directory = "../output/SPCG_promoters/fly_factor_survey"
+fimo_directory = "../output/SPCG_regulatory_regions/fly_factor_survey"
 folder_list = os.listdir(fimo_directory)
 folder_list = [x for x in folder_list if x != '.DS_Store']
 folder_list = [x for x in folder_list if x != 'motif_database.txt']
@@ -91,10 +72,10 @@ for tmp_folder in folder_list:
 TF_df[TF_df > 1] = 1
 TF_df.index = conversion_df.loc[TF_df.index, 0] + " (" + conversion_df.loc[TF_df.index, 1] + ")"
 
-TF_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_combined_hits_0.0001.csv'))
+TF_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_combined_hits.csv'))
 
 # summarize the results 
 TF_df.sum(axis = 1).sort_values()
 prop_df = pd.DataFrame(data = np.array([TF_df.index, TF_df.sum(axis = 1)]).T, columns = ['TF_motifs', 'num_SPCGs'])
 prop_df = prop_df.sort_values('num_SPCGs')
-prop_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_combined_hits_0.0001_prop.csv'))
+prop_df.to_csv(os.path.join(out_path, 'flyfactorsurvey_combined_hits_prop.csv'))
