5220c726abed2d40ac8fc6538e38684fb2811623
dpeng28 <dpeng5@jh.edu>
2024-06-25 16:42:54 -0400

    added cateogirzation of DE genes

--- CrebA_ChIP_Analysis/workflow/enrichment_categorized_DE_genes.R
+++ CrebA_ChIP_Analysis/workflow/enrichment_categorized_DE_genes.R
@@ -1,34 +1,39 @@
-TARGET_dir = file.path("results", ANALYSIS_VERSION, "enrichment_categorized_DE_genes")
+library(enrichR)
+library(ggplot2)
+
+enrichR::setEnrichrSite('FlyEnrichr')
+
+TARGET_dir = file.path("../output/enrichment_categorized_DE_genes")
 dir.create(TARGET_dir, recursive = TRUE)
 
-categorized_DE = read.csv("accessory_data/CrebA_ChIP_Analysis/output/coordinates_bound_regions/all_bounded_genes.csv", sep = '\t')
+##### get the DE genes ###### 
+down_DE_genes = read.csv("../output/find_bound_DE_genes/down_DE.csv", row.names = 1)
+up_DE_genes = read.csv("../output/find_bound_DE_genes/up_DE.csv", row.names = 1)
 
-down_genes = categorized_DE[categorized_DE$General.Type == 'Down', 'target_genes']
+# only select the down DE genes that are in sc and bound 
+down_DE_genes = down_DE_genes[down_DE_genes$bound == 'True' & down_DE_genes$SC_DE == 'True', ]
+up_DE_genes = up_DE_genes[up_DE_genes$bound == 'True' & up_DE_genes$SC_DE == 'True', ]
+
+##### categorize them ##### 
+down_genes = down_DE_genes[, 'genes']
 enriched <- enrichR::enrichr(down_genes, 'GO_Biological_Process_2018')
 down_enriched_df = enriched$GO_Biological_Process_2018
 write.csv(down_enriched_df, file = file.path(TARGET_dir, 'down_genes_enrichment.csv'))
 
-target_genes = categorized_DE[categorized_DE$General.Type == 'Up', 'target_genes']
-enriched <- enrichR::enrichr(target_genes, 'GO_Biological_Process_2018')
+up_genes = up_DE_genes[, 'genes']
+enriched <- enrichR::enrichr(up_genes, 'GO_Biological_Process_2018')
 up_enriched_df = enriched$GO_Biological_Process_2018
 write.csv(up_enriched_df, file = file.path(TARGET_dir, 'up_genes_enrichment.csv'))
 
-target_genes = categorized_DE[categorized_DE$General.Type == 'Static', 'target_genes']
-enriched <- enrichR::enrichr(target_genes, 'GO_Biological_Process_2018')
-static_enriched_df = enriched$GO_Biological_Process_2018
-write.csv(static_enriched_df, file = file.path(TARGET_dir, 'static_genes_enrichment.csv'))
-
+##### look at the up and down genes #####
 down_enriched_df$logpval = -log(down_enriched_df$Adjusted.P.value)
 down_enriched_df$type = 'Down'
 up_enriched_df$logpval = -log(up_enriched_df$Adjusted.P.value)
 up_enriched_df$type = 'Up'
-static_enriched_df$logpval = -log(static_enriched_df$Adjusted.P.value)
-static_enriched_df$type = 'Static'
 
 down_enriched_df = down_enriched_df[order(down_enriched_df$Adjusted.P.value), ]
 up_enriched_df = up_enriched_df[order(up_enriched_df$Adjusted.P.value), ]
-static_enriched_df = static_enriched_df[order(static_enriched_df$Adjusted.P.value), ]
-big_df = rbind(down_enriched_df[1:5, ], static_enriched_df[1:5, ], up_enriched_df[1:5, ])
+big_df = rbind(down_enriched_df[1:5, ], up_enriched_df[1:5, ])
 
 p <- ggplot(data = big_df, aes(y = reorder(Term, logpval), x = logpval, fill = type)) +
   geom_bar(stat="identity") +
