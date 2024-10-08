51c11d137af209623a113a08539ffd1076f994ff
dpeng28 <dpeng5@jh.edu>
2024-10-08 16:15:04 -0400

    added scripts for finding SPCG

--- CrebA_ChIP_Analysis/workflow/check_bound_SPCGs.py
+++ CrebA_ChIP_Analysis/workflow/check_bound_SPCGs.py
@@ -1,79 +1,10 @@
 import pandas as pd
 import numpy as np 
-import os
 
-###### to make the working directory ###### 
-output_path = "../output/find_bound_SPCGs"
-if os.path.isdir(output_path) == False: 
-    os.makedirs(output_path)    
+spcg_1000 = pd.read_csv("../output/find_bound_SPCGs/spcg_match_1000.csv")
+spcg_1000 = spcg_1000.loc[spcg_1000['fkh_sage_intersect_peaks_bound'] == False, :]
 
-##### this is the function to reproduce data ##### 
-def find_bound_in_region_genes(peak_path, DE_path):
-    narrow_peaks = pd.read_csv(peak_path, index_col=0)
-    concatenated_array = np.concatenate((np.array(narrow_peaks['nearest_gene_1'].dropna()), np.array(narrow_peaks['nearest_gene_2'].dropna())))
-    concatenated_array = np.unique(concatenated_array)
-    concatenated_array = list(concatenated_array)
-    for element in concatenated_array:
-        # Split the string by whitespace
-        split_elements = element.split(",")
-        if len(split_elements) > 1:
-            concatenated_array.extend(split_elements)
-            concatenated_array.remove(element)
-    bound_genes = concatenated_array
-    output_df = pd.DataFrame(index = ['manual_bound_all', 'manual_unbound_all', 'manual_bound_SG', 'manual_unbound_SG'], columns = ['down_genes', 'up_genes', 'static_genes'])
-    for i in range(0, 3): 
-        if i == 0: 
-            type_gene = 'down_genes'
-        elif i == 1: 
-            type_gene = 'up_genes'
-        elif i == 2: 
-            type_gene = 'static_genes'
+spcg_2000 = pd.read_csv("../output/find_bound_SPCGs/spcg_match_2000.csv")
+spcg_2000 = spcg_2000.loc[spcg_2000['fkh_sage_intersect_peaks_bound'] == False, :]
 
-        DE_df = pd.read_excel(DE_path, sheet_name = i)
-        DE_df['bound_status'] = 0
-        DE_df.loc[DE_df['target_genes'].isin(bound_genes), 'bound_status'] = 1
-
-        sub_DE = DE_df.loc[[True if 'Unbound' in x else False for x in DE_df['Bound Status']], :]
-        output_df.loc['manual_unbound_all', type_gene] = np.sum(sub_DE['bound_status']) / sub_DE.shape[0]
-
-        sub_DE = sub_DE.loc[sub_DE['Salivary Gland'] != 'other', :]
-        output_df.loc['manual_unbound_SG', type_gene] = np.sum(sub_DE['bound_status']) / sub_DE.shape[0]
-
-        sub_DE = DE_df.loc[[True if 'Bound' in x else False for x in DE_df['Bound Status']], :]
-        output_df.loc['manual_bound_all', type_gene] = np.sum(sub_DE['bound_status']) / sub_DE.shape[0]
-
-        sub_DE = sub_DE.loc[sub_DE['Salivary Gland'] != 'other', :]
-        output_df.loc['manual_bound_SG', type_gene] = np.sum(sub_DE['bound_status']) / sub_DE.shape[0]
-
-    return output_df 
-
-spcg_df = pd.read_excel('../input/SPCG_files/SPCG List.xlsx')
-conversion_tab = pd.read_csv("../input/flybase_gene_conversion/conversion_tab.csv")
-conversion_tab.index = conversion_tab['flybase']
-sub_conversion_tab = conversion_tab.loc[spcg_df['Drosophila FBgn'], :]
-spcg_df['new_gene_id'] = np.array(sub_conversion_tab['gene_names'])
-
-###### +/- 1kb fkh sage intersect genes ######
-peak_path = "../output/match_nearest_gene/fkh_sage_intersect_genes.csv"
-DE_path = "../input/manual_curated_DE_genes/manual_curated_DE.xlsx"
-output_df = find_bound_in_region_genes(peak_path, DE_path)
-print(output_df)
-
-narrow_peaks = pd.read_csv(peak_path, index_col=0)
-concatenated_array = np.concatenate((np.array(narrow_peaks['nearest_fly_id_1'].dropna()), np.array(narrow_peaks['nearest_fly_id_2'].dropna())))
-concatenated_array = np.unique(concatenated_array)
-concatenated_array = list(concatenated_array)
-for element in concatenated_array:
-    # Split the string by whitespace
-    split_elements = element.split(",")
-    if len(split_elements) > 1:
-        concatenated_array.extend(split_elements)
-        concatenated_array.remove(element)
-
-bound_genes = concatenated_array
-
-spcg_df['fkh_sage_intersect_peaks_bound'] = False
-spcg_df.loc[spcg_df['Drosophila FBgn'].isin(bound_genes), 'fkh_sage_intersect_peaks_bound'] = True
-np.sum(spcg_df['Drosophila FBgn'].isin(bound_genes)) / spcg_df.shape[0]
-
-sub_narrow = narrow_peaks.loc[narrow_peaks['nearest_fly_id_1'].isin(spcg_df['Drosophila FBgn']), :]
+np.setdiff1d(spcg_1000['Drosophila Gene'], spcg_2000['Drosophila Gene'])
---
+++ CrebA_ChIP_Analysis/workflow/find_bound_SPCGs.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np 
+import os
+import argparse 
+
+parser = argparse.ArgumentParser(description='find bound SPCGs')
+parser.add_argument('--input', type=str, help = 'input file')
+parser.add_argument('--output', type=str, help = 'output file')
+args = parser.parse_args()
+
+spcg_df = pd.read_excel('../input/SPCG_files/SPCG List.xlsx')
+conversion_tab = pd.read_csv("../input/flybase_gene_conversion/conversion_tab.csv")
+conversion_tab.index = conversion_tab['flybase']
+sub_conversion_tab = conversion_tab.loc[spcg_df['Drosophila FBgn'], :]
+spcg_df['new_gene_id'] = np.array(sub_conversion_tab['gene_names'])
+
+###### +/- 1kb fkh sage intersect genes ######
+peak_path = args.input
+output_path = args.output
+
+narrow_peaks = pd.read_csv(peak_path, index_col=0)
+concatenated_array = np.concatenate((np.array(narrow_peaks['nearest_fly_id_1'].dropna()), np.array(narrow_peaks['nearest_fly_id_2'].dropna())))
+concatenated_array = np.unique(concatenated_array)
+concatenated_array = list(concatenated_array)
+for element in concatenated_array:
+    # Split the string by whitespace
+    split_elements = element.split(",")
+    if len(split_elements) > 1:
+        concatenated_array.extend(split_elements)
+        concatenated_array.remove(element)
+
+bound_genes = concatenated_array
+
+spcg_df['fkh_sage_intersect_peaks_bound'] = False
+spcg_df.loc[spcg_df['Drosophila FBgn'].isin(bound_genes), 'fkh_sage_intersect_peaks_bound'] = True
+spcg_df.to_csv(output_path, index=False)
